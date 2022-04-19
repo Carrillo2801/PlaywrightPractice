d367e3360ab7827a7c1e5c86af0dd033ab1a4ecc
Jorge Carrillo <e.carrillo.cirerol@gmail.com>
2022-04-18 21:29:26 -0500

    Updated configuration file

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,9 +35,10 @@ const config: PlaywrightTestConfig = {
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-    viewport: null,
+    headless: false,
+    screenshot: 'only-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
